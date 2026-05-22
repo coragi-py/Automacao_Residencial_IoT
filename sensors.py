dafe572c656be77a023cf28df5ad32297840cfde
coragi-py <fabioyuuki8@gmail.com>
2026-05-22 17:21:11 -0300

    feat: cria modulo de leitura para DHT22, LDR, PIR e Botao

---
+++ sensors.py
@@ -0,0 +1,32 @@
+from machine import Pin, ADC
+import dht
+
+# Mapeamento estrito de hardware (GPIOs da Etapa 1)
+dht_sensor = dht.DHT22(Pin(15))
+ldr_sensor = ADC(Pin(34))
+ldr_sensor.atten(ADC.ATTN_11V) # Configuração para leitura até 3.3V
+pir_sensor = Pin(27, Pin.IN)
+btn_sensor = Pin(14, Pin.IN, Pin.PULL_UP)
+
+def read_sensors():
+    try:
+        dht_sensor.measure()
+        temp = dht_sensor.temperature()
+        hum = dht_sensor.humidity()
+    except OSError:
+        # Tratamento caso o sensor falhe na leitura
+        temp = 0.0
+        hum = 0.0
+        
+    ldr_val = ldr_sensor.read()
+    pir_val = pir_sensor.value()
+    # O botão com pull-up interno retorna 0 quando pressionado e 1 quando solto
+    btn_val = btn_sensor.value() 
+    
+    return {
+        "temp": temp,
+        "hum": hum,
+        "ldr": ldr_val,
+        "pir": pir_val,
+        "btn": btn_val
+    }
